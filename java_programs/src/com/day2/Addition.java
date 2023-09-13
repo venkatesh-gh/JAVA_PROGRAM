51a40822c53b16d2ad30a5af369f8249c1b5b3e5
ELCOT <ELCOT@Lenovo>
2023-09-13 18:18:49 +0530

    done

--- java_programs/src/com/day2/Addition.java
+++ java_programs/src/com/day2/Addition.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Addition {
 
 	public static void main(String[] args) {
-		 TODO Auto-generated method stub
+		 //TODO Auto-generated method stub
 		int a,b;
 		System.out.println("enter the value");
 		Scanner s=new Scanner(System.in);
 		a=s.nextInt();
 		b=s.nextInt();
 		int c=a+b;
-		System.out.println("the addition of a="+a+" and b="+b+" is = "+c);
+		System.out.println("the addition of a= "+a+" and b= "+b+" is = "+c);
 
 	}
 
---
+++ java_programs/src/com/day3/EXAMPLE.java
@@ -0,0 +1,10 @@
+package com.day3;
+
+public class EXAMPLE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("wecome");
+	}
+
+}
