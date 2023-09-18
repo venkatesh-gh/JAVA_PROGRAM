c11b699e9348121f27c0a53387ff97d2b6463ffe
ELCOT <ELCOT@Lenovo>
2023-09-18 18:38:38 +0530

    yes

--- java_programs/src/com/day3/EXAMPLE.java
+++ java_programs/src/com/day3/EXAMPLE.java
@@ -4,7 +4,14 @@ public class EXAMPLE {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("wecome");
+	
+//		int a=10,b=20,c=10;
+////		++a;
+////		c=++a + b + a--;
+////		System.out.println(c);
+//		int a=11,b=20,c=44;
+//		int d=c++ + a + b--;
+//		System.out.println(d);
 	}
 
 }
---
+++ java_programs/src/com/test_case1/Test_case1.java
@@ -0,0 +1,17 @@
+package com.test_case1;
+
+public class Test_case1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
